abdecbd4a3c834824ab00fddb93019fd25e86743
7Liu <1169344380@qq.com>
2026-07-13 15:49:07 +0800

    优化有效括号题解中的注释和逻辑结构

--- 栈/20-有效的括号.py
+++ 栈/20-有效的括号.py
@@ -10,26 +10,30 @@ class Solution(object):
         但是复杂度为N-> 栈
         开始遍历入栈。。若遇到相邻的配对括号则出栈
         遇到左元素入栈，遇到右元素弹出比较。
+
+        对称结构，配对消除，先进后出：栈
         """
         stack = []
         mapping = {")": "(", "}": "{", "]": "["}
+        
         for char in s:
             if char in {"(", "{", "["}:
                 stack.append(char)
+
             else:
-                # 出栈前先判断
                 if not stack:
                     return False
-                
+
                 top_ele = stack.pop()
-                # 映射关系，直接使用哈希
                 if mapping[char] != top_ele:
                     return False
-        
-        if not stack:
-            return True
-        else: 
+                
+        if stack:
             return False
+        else:
+            return True
+                
 
+            
 
         
